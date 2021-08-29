c1b8d9a30c5c8f4e5bbe9b0bd05d5ab4000c16ac
MishkaRogachev <mishkarogachev@gmail.com>
2021-08-29 16:35:37 +0300

    Add common tmi constants

---
+++ include/common_tmi.h
@@ -0,0 +1,63 @@
+#ifndef COMMON_TMI_H
+#define COMMON_TMI_H
+
+namespace tmi
+{
+// System
+constexpr char state[] = "state";
+constexpr char type[] = "type";
+constexpr char online[] = "online";
+
+// Arming
+constexpr char armed[] = "armed";
+constexpr char setArmed[] = "setArmed";
+constexpr char setArmedStatus[] = "setArmedStatus";
+
+// Modes
+constexpr char mode[] = "mode";
+constexpr char modes[] = "modes";
+constexpr char setMode[] = "setMode";
+constexpr char setModeStatus[] = "setModeStatus";
+
+// Attitude
+constexpr char pitch[] = "pitch";
+constexpr char roll[] = "roll";
+constexpr char yaw[] = "yaw";
+constexpr char heading[] = "heading";
+constexpr char course[] = "course";
+constexpr char desiredRoll[] = "desiredRoll";
+constexpr char desiredPitch[] = "desiredPitch";
+constexpr char desiredHeading[] = "desiredHeading";
+
+// Altitudes
+constexpr char altitudeAmsl[] = "altitudeAmsl";
+constexpr char altitudeRelative[] = "altitudeRelative";
+constexpr char altitudeTerrain[] = "altitudeTerrain";
+constexpr char climb[] = "climb";
+
+// Speeds
+constexpr char gs[] = "gs";
+constexpr char ias[] = "ias";
+constexpr char tas[] = "tas";
+
+// Position
+constexpr char latitude[] = "latitude";
+constexpr char longitude[] = "longitude";
+constexpr char x[] = "x";
+constexpr char y[] = "y";
+
+// Home
+constexpr char homeLatitude[] = "homeLatitude";
+constexpr char homeLongitude[] = "homeLongitude";
+constexpr char homeAltitude[] = "homeAltitude";
+
+// Mission
+constexpr char wp[] = "wp";
+constexpr char setWp[] = "setWp";
+constexpr char setWpStatus[] = "setWpStatus";
+constexpr char wpDistance[] = "wpDistance";
+constexpr char targetBearing[] = "targetBearing";
+
+} // namespace tmi
+
+#endif // COMMON_TMI_H
