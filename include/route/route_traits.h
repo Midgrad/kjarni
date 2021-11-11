36f0b81786397c8667d06e8c84c6f89fca5feccb
MishkaRogachev <mishkarogachev@gmail.com>
2021-11-11 19:38:26 +0300

    Add acceptRadius and relativeAlt default false

--- include/route/route_traits.h
+++ include/route/route_traits.h
@@ -32,13 +32,15 @@ constexpr float maxDistance = 1000000.0;
 constexpr float maxRadius = 1000000.0;
 constexpr int maxTime = 100000; // 86400 sec in a day
 
-const Parameter relativeAlt = { "relative_alt", TR_ROUTE("Rel.alt."), true };
+const Parameter relativeAlt = { "relative_alt", TR_ROUTE("Rel.alt."), false };
 const Parameter distance = {
     "distance", TR_ROUTE("Distance"), Parameter::Real, 0.0, 0.0, maxDistance, 1
 };
 const Parameter time = { "time", TR_ROUTE("Time"), Parameter::Int, 0, 0, maxTime };
 const Parameter yaw = { "yaw", TR_ROUTE("Yaw"), Parameter::Real, qQNaN(), 0, 360 };
 const Parameter pitch = { "pitch", TR_ROUTE("Pitch"), Parameter::Real, 15.0, -90.0, 90.0 };
+const Parameter acceptRadius = { "accept_radius", TR_ROUTE("Accept. radius"), Parameter::Real, 0, 0,
+                                 maxRadius };
 const Parameter radius = { "radius", TR_ROUTE("Radius"), Parameter::Real, 0, 0, maxRadius };
 } // namespace route
 } // namespace md::domain
