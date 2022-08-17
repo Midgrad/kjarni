a325b59671c5583a90c8a719ac8c24d007328e7f
MishkaRogachev <mishkarogachev@gmail.com>
2022-08-17 16:05:44 +0400

    Store communication links in database

--- src/sql/sqlite_schema.cpp
+++ src/sql/sqlite_schema.cpp
@@ -43,13 +43,58 @@ void SqliteSchema::setup()
     query.exec("CREATE TABLE schema_version (version STRING NOT NULL UNIQUE)");
 
     // Comm Links
+    query.exec("CREATE TABLE comm_link_types ("
+               "id STRING NOT NULL UNIQUE, "
+               "name STRING);");
+
+    query.exec("CREATE TABLE comm_link_params ("
+               "id STRING NOT NULL UNIQUE, "
+               "name STRING, "
+               "minValue STRING, "
+               "maxValue STRING, "
+               "step STRING, "
+               "variants TEXT, "
+               "type STRING );");
+
+    query.exec("CREATE TABLE comm_link_type_params ("
+               "type STRING, "
+               "param STRING, "
+               "defaultValue STRING, "
+               "FOREIGN KEY(type) REFERENCES comm_link_types(id) ON DELETE CASCADE, "
+               "FOREIGN KEY(param) REFERENCES comm_link_params(id) ON DELETE CASCADE);");
+
     query.exec("CREATE TABLE comm_links ("
                "id UUID PRIMARY KEY NOT NULL, "
                "name STRING, "
                "params TEXT, "
                "protocol STRING, "
                "connected BOOL, "
-               "type STRING);");
+               "type STRING, "
+               "FOREIGN KEY(type) REFERENCES comm_link_types(id) ON DELETE CASCADE);");
+
+    query.exec("INSERT INTO comm_link_types (id, name) VALUES (\'udp\', \'UDP\')");
+    query.exec("INSERT INTO comm_link_types (id, name) VALUES (\'tcp\', \'TCP\')");
+    query.exec("INSERT INTO comm_link_types (id, name) VALUES (\'uart\', \'UART\')");
+
+    query.exec("INSERT INTO comm_link_params (id, name, type, minValue, maxValue) "
+               "VALUES (\'port\', \'Port\', \'int\', 1024, 65535)");
+    query.exec("INSERT INTO comm_link_params (id, name, type) "
+               "VALUES (\'address\', \'Address\', \'string\')");
+    query.exec("INSERT INTO comm_link_params (id, name, type) "
+               "VALUES (\'device\', \'Device\', \'string\')");
+    query.exec("INSERT INTO comm_link_params (id, name, type) "
+               "VALUES (\'baud_rate\', \'Baud rate\', \'int\')");
+
+    query.exec("INSERT INTO comm_link_type_params (type, param, defaultValue) "
+               "VALUES (\'udp\', \'port\', 14550)");
+    query.exec("INSERT INTO comm_link_type_params (type, param, defaultValue) "
+               "VALUES (\'tcp\', \'port\', 5760)");
+    query.exec("INSERT INTO comm_link_type_params (type, param, defaultValue) "
+               "VALUES (\'tcp\', \'address\', \'127.0.0.1\')");
+    query.exec("INSERT INTO comm_link_type_params (type, param) "
+               "VALUES (\'uart\', \'device\')");
+    query.exec("INSERT INTO comm_link_type_params (type, param, defaultValue) "
+               "VALUES (\'uart\', \'baud_rate\', 57600)");
 
     // Vehicles
     query.exec("CREATE TABLE vehicles ("
